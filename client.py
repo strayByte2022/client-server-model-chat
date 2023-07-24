54b89a7ee24d5424a59cc03c74cd7afcbea9111b
l1nhx <linhhandsom@gmail.com>
2023-07-24 23:41:59 +0700

    first commit, with basics client-server functions added

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+import threading
+import time
+
+HEADER = 64 
+PORT = 3472 # netstat -ano -> choose listening ports
+# automatically get the addr that the host run on
+host_name = socket.gethostname()
+SERVER = socket.gethostbyname(host_name) 
+ADDR = (SERVER,PORT)
+FORMAT = 'utf-8'
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send_message(msg):
+    message = msg.encode(FORMAT)
+    message_length = len(message)
+    send_length = str(message_length).encode(FORMAT)
+    send_length += b' '*(HEADER-len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+send_message('Hello')
---
+++ server.py
@@ -0,0 +1,43 @@
+import socket
+import threading
+import time
+
+HEADER = 64 
+PORT = 3472 # netstat -ano -> choose listening ports
+# automatically get the addr that the host run on
+host_name = socket.gethostname()
+SERVER = socket.gethostbyname(host_name) 
+ADDR = (SERVER,PORT)
+FORMAT = 'utf-8'
+
+# bind address to socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #streaming data through ipv4
+server.bind(ADDR)
+
+
+def handle_client(connection, address):
+    print(f'[NEW CONNECTION] {address} connected.')
+    connected = True
+    while connected:
+        message_length = connection.recv(HEADER).decode(FORMAT) # convert bytes format -> string 
+        if message_length:
+            message_length = int(message_length)
+
+            message = connection.recv(message_length).decode(FORMAT)
+            if message == '!disconnect':
+                connected = False
+        
+            print(f'[{address}] {message}')
+    connection.closed()
+
+def start():
+    server.listen()
+    print(f'[LISTENING] Server is listening on {SERVER}')
+    while True:
+        connection,address = server.accept()
+        thread = threading.Thread(target=handle_client,args=(connection,address))
+        thread.start()
+        print(f'[ACTIVE CONNECTIONS]{threading.active_count()-1}')
+    
+print('[STARTING] server is starting')
+start()
